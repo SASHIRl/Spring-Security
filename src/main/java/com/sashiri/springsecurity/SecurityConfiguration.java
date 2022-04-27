07c62d584000f79b34926cc70f4ae7818114d23c
Diego Marques <marquesashiri@gmail.com>
2022-04-27 18:41:09 -0300

    first commit

---
+++ src/main/java/com/sashiri/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.sashiri.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//Diz para o spring que essa é uma classe que contém configuração.
+@Configuration
+//Diz que além de ser uma classe de configuração, contém segurança para web.
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    //Faz um enconder do password
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Qualquer request feito autenticado vai para o formLogin
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder()
+                .encode("password")).authorities("USER");
+    }
+}
